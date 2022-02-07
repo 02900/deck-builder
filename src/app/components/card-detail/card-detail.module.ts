af070b680158f9f9b5cf481301b162c8c0013c64
Juan Ortiz <juan.ortiz@kavak.com>
2022-02-07 14:09:01 -0600

    feat: minor improves on card-detail component

--- src/app/components/card-detail/card-detail.module.ts
+++ src/app/components/card-detail/card-detail.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardModule } from '@components/card/card.module';
+import { ItemPairValueModule } from '@components/item-pair-value/item-pair-value.module';
 import { CardDetailComponent } from './card-detail.component';
 
 @NgModule({
   declarations: [CardDetailComponent],
-  imports: [CommonModule, CardModule],
+  imports: [CommonModule, CardModule, ItemPairValueModule],
   exports: [CardDetailComponent],
 })
 export class CardDetailModule { }
