53f58340c710712cf70ad360ce48d3f5d1f25a17
Juan Ortiz <juan.ortiz@kavak.com>
2022-02-14 13:58:56 -0600

    feat: app layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from "@angular/common/http";
-import { CardModule } from "@components/card/card.module";
+import { HttpClientModule } from '@angular/common/http';
+import { CardDetailModule } from '@components/card-detail/card-detail.module';
+import { DeckViewerModule } from '@components/deck-viewer/deck-viewer.module';
+import { CardCatalogModule } from '@components/card-catalog/card-catalog.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule, CardModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    CardDetailModule,
+    DeckViewerModule,
+    CardCatalogModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
